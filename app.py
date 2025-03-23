ddc7bad308a11d0acf7f8e4d9f8bb0d7c8a478ce
uu4220 <jtbmnc@gmail.com>
2025-03-23 03:18:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,7 @@ def index():
 
 if __name__ == "__main__":
     os.makedirs("static", exist_ok=True)
-    app.run(debug=True)
+ import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
