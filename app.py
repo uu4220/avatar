c7ca35240ff753a8a7b8d8c74224e2367242dcda
uu4220 <jtbmnc@gmail.com>
2025-04-05 22:23:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,10 @@ def get_avatar():
     return send_file("static/mon_avatar.png", mimetype="image/png")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
