ba6f64b87d5b8be42802459d50c7d9e2f120c415
uu4220 <jtbmnc@gmail.com>
2025-04-05 22:06:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template, request
-from generator import generer_avatar
+from flask import Flask, render_template, request, send_file
+from generator_interaction_label import generer_avatar
 import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    avatar_generated = False
     if request.method == "POST":
         couleur = request.form.get("couleur")
-        emotion = request.form.get("emotion")
+        etat = request.form.get("etat")
         element = request.form.get("element")
-        generer_avatar(couleur, emotion, element)
-    return render_template("index.html")
+        interaction = request.form.get("interaction")
+
+        # 调用角色生成函数
+        img = generer_avatar(couleur, etat, element, interaction)
+        img_path = os.path.join("static", "mon_avatar.png")
+        img.save(img_path)
+        avatar_generated = True
+
+    return render_template("index.html", avatar_generated=avatar_generated)
+
+@app.route("/avatar")
+def get_avatar():
+    return send_file("static/mon_avatar.png", mimetype="image/png")
 
 if __name__ == "__main__":
-    os.makedirs("static", exist_ok=True)
-import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
 
